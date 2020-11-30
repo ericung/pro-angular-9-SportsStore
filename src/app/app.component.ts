030dee1901c97d1941cec27d00be144127b72e07
Eric Ung <ungericw@tutanota.com>
2020-11-30 12:48:23 -0600

    11/30/2020 routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
 
-/*
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-*/
-
 @Component({
   selector: "app",
-  template: `<store></store>`
+  template: "<router-outlet></router-outlet>"
 })
 export class AppComponent {
   title = 'SportsStore';
